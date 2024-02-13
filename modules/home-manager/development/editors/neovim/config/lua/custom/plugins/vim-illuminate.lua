369b3dbf2ced775ba89b1ba7cd45481ef9e4a318
shaun-mathew <shaunmathew123@gmail.com>
2024-02-13 12:05:51 -0500

    update vim-illuminate mappings

--- modules/home-manager/development/editors/neovim/config/lua/custom/plugins/vim-illuminate.lua
+++ modules/home-manager/development/editors/neovim/config/lua/custom/plugins/vim-illuminate.lua
@@ -1,41 +1,38 @@
 return {
-    "RRethy/vim-illuminate",
-    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-    opts = {
-        delay = 200,
-        large_file_cutoff = 2000,
-        large_file_overrides = {
-            providers = { "lsp" },
-        },
+  'RRethy/vim-illuminate',
+  event = { 'BufReadPost', 'BufWritePost', 'BufNewFile' },
+  opts = {
+    delay = 200,
+    large_file_cutoff = 2000,
+    large_file_overrides = {
+      providers = { 'lsp' },
     },
-    config = function(_, opts)
-        require("illuminate").configure(opts)
+  },
+  config = function(_, opts)
+    require('illuminate').configure(opts)
 
+    local buffer = vim.api.nvim_get_current_buf()
+    vim.keymap.set('n', '<leader>]', function()
+      require('illuminate').goto_next_reference()
+    end, { desc = 'Goto next reference', buffer = buffer })
 
-        local buffer = vim.api.nvim_get_current_buf()
-        vim.keymap.set("n", "<leader>]",
-            function()
-                require("illuminate").goto_next_reference()
-            end, { desc = "Goto next reference", buffer = buffer })
+    vim.keymap.set('n', '<leader>[', function()
+      require('illuminate').goto_prev_reference()
+    end, { desc = 'Goto prev reference', buffer = buffer })
 
-        vim.keymap.set("n", "<leader>[",
-            function()
-                require("illuminate").goto_prev_reference()
-            end, { desc = "Goto prev reference", buffer = buffer })
-
-        -- vim.api.nvim_create_autocmd("FileType", {
-        --     callback = function()
-        --         local buffer = vim.api.nvim_get_current_buf()
-        --         vim.keymap.set("n", "]]",
-        --             function()
-        --                 require("illuminate").goto_next_reference()
-        --             end, { desc = "Goto prev reference", buffer = buffer })
-        --
-        --         vim.keymap.set("n", "]]",
-        --             function()
-        --                 require("illuminate").goto_prev_reference()
-        --             end, { desc = "Goto prev reference", buffer = buffer })
-        --     end,
-        -- })
-    end,
+    -- vim.api.nvim_create_autocmd("FileType", {
+    --     callback = function()
+    --         local buffer = vim.api.nvim_get_current_buf()
+    --         vim.keymap.set("n", "]]",
+    --             function()
+    --                 require("illuminate").goto_next_reference()
+    --             end, { desc = "Goto prev reference", buffer = buffer })
+    --
+    --         vim.keymap.set("n", "]]",
+    --             function()
+    --                 require("illuminate").goto_prev_reference()
+    --             end, { desc = "Goto prev reference", buffer = buffer })
+    --     end,
+    -- })
+  end,
 }
