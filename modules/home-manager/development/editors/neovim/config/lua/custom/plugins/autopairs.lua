f2ffa2e0afe2ab27ae7c091ab6c10c547aa7f596
shaun-mathew <shaunmathew123@gmail.com>
2024-02-13 11:51:07 -0500

    add autopairs

--- modules/home-manager/development/editors/neovim/config/lua/custom/plugins/autopairs.lua
+++ modules/home-manager/development/editors/neovim/config/lua/custom/plugins/autopairs.lua
@@ -1,7 +1,7 @@
 return {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    opts = {
-        fast_wrap = {}
-    }
+  'windwp/nvim-autopairs',
+  event = 'InsertEnter',
+  opts = {
+    fast_wrap = {},
+  },
 }
