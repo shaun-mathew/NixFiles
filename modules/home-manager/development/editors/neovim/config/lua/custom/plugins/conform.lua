a99e056e381ddaf334c1e03fa850c10c4d2a59a3
shaun-mathew <shaunmathew123@gmail.com>
2024-02-13 12:00:36 -0500

    trim whitespace and newlines for generic files

--- modules/home-manager/development/editors/neovim/config/lua/custom/plugins/conform.lua
+++ modules/home-manager/development/editors/neovim/config/lua/custom/plugins/conform.lua
@@ -18,9 +18,9 @@ return {
     -- Define your formatters
     formatters_by_ft = {
       lua = { 'stylua' },
-      python = { 'isort', 'black' },
       python = { 'ruff_format' },
       javascript = { { 'prettierd', 'prettier' } },
+      ['_'] = { 'trim_whitespace', 'trim_newlines' },
     },
     -- Set up format-on-save
     format_on_save = { timeout_ms = 500, lsp_fallback = true },
