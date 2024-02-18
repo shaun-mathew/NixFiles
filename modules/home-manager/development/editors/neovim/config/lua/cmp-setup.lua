92b48d3e8b84366e02033d878864853982b0fdd0
shaun-mathew <shaunmathew123@gmail.com>
2024-02-17 22:42:00 -0500

    add buffer as lsp source

--- modules/home-manager/development/editors/neovim/config/lua/cmp-setup.lua
+++ modules/home-manager/development/editors/neovim/config/lua/cmp-setup.lua
@@ -47,6 +47,7 @@ cmp.setup {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
     { name = 'path' },
+    { name = 'buffer' },
   },
 }
 
--- modules/home-manager/development/editors/neovim/config/lua/lazy-plugins.lua
+++ modules/home-manager/development/editors/neovim/config/lua/lazy-plugins.lua
@@ -56,6 +56,7 @@ require('lazy').setup({
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-cmdline',
+      'hrsh7th/cmp-buffer',
 
       -- Adds a number of user-friendly snippets
       'rafamadriz/friendly-snippets',
