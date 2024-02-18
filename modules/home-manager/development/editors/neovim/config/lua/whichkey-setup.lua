673909aa30d0d8f23ace76b727dd7f38aaa4f04b
shaun-mathew <shaunmathew123@gmail.com>
2024-02-18 15:27:33 -0500

    remove unnecessary comments in whichkey stub file

--- modules/home-manager/development/editors/neovim/config/lua/whichkey-setup.lua
+++ modules/home-manager/development/editors/neovim/config/lua/whichkey-setup.lua
@@ -1,12 +1,9 @@
 -- document existing key chains
 require('which-key').register {
   ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-  -- ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-  -- ['<leader>g'] = { name = '[G] Commands', _ = 'which_key_ignore' },
   ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
   ['<leader>gh'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
   ['<leader>l'] = { name = '[L]sp Actions', _ = 'which_key_ignore' },
-  -- ['<leader>lw'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
   ['<leader>f'] = { name = '[F]ind', _ = 'which_key_ignore' },
   ['<leader>t'] = { name = '[T]erminal', _ = 'which_key_ignore' },
   ['<leader>tr'] = { name = '[T]erminal Create', _ = 'which_key_ignore' },
