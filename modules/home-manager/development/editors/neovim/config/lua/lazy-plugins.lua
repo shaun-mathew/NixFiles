2d5b8704ef46dd87c5289c32cb2ae566f9f1d0e5
shaun-mathew <shaunmathew123@gmail.com>
2024-02-18 20:40:53 -0500

    add noice

--- modules/home-manager/development/editors/neovim/config/lua/lazy-plugins.lua
+++ modules/home-manager/development/editors/neovim/config/lua/lazy-plugins.lua
@@ -20,6 +20,44 @@ require('lazy').setup({
     'stevearc/dressing.nvim',
     opts = {},
   },
+
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {
+      lsp = {
+        progress = {
+          enabled = false,
+        },
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+          ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+        },
+      },
+
+      messages = {
+        enabled = false,
+      },
+      notify = {
+        enabled = false,
+      },
+      popupmenu = {
+        enabled = false,
+      },
+      -- you can enable a preset for easier configuration
+      presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        lsp_doc_border = true, -- add a border to hover docs and signature help
+      },
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      'MunifTanjim/nui.nvim',
+    },
+  },
   {
     -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
@@ -37,10 +75,8 @@ require('lazy').setup({
       {
         'kosayoda/nvim-lightbulb',
         opts = {
-          {
-            autocmd = {
-              enabled = true,
-            },
+          autocmd = {
+            enabled = true,
           },
         },
       },
