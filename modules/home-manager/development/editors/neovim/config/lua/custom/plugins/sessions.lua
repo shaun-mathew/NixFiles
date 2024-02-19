b6c85ad86893cee4cbdc060cab0e7df6b8e28af2
shaun-mathew <shaunmathew123@gmail.com>
2024-02-19 14:44:24 -0500

    replace session plugin

--- modules/home-manager/development/editors/neovim/config/lua/custom/plugins/sessions.lua
+++ modules/home-manager/development/editors/neovim/config/lua/custom/plugins/sessions.lua
@@ -1,11 +1,41 @@
 return {
-  'rmagatti/auto-session',
+  'stevearc/resession.nvim',
   opts = {
-    auto_session_use_git_branch = true,
-    auto_restore_enabled = true,
-    auto_save_enabled = true,
+    autosave = {
+      enabled = true,
+      interval = 60,
+      notify = false,
+    },
   },
   config = function(_, opts)
-    require('auto-session').setup(opts)
+    local resession = require 'resession'
+
+    resession.setup(opts)
+    vim.keymap.set('n', '<leader>ss', resession.save)
+    vim.keymap.set('n', '<leader>sl', resession.load)
+    vim.keymap.set('n', '<leader>sd', resession.delete)
+
+    local function get_session_name()
+      local name = vim.fn.getcwd()
+      local branch = vim.trim(vim.fn.system 'git branch --show-current')
+      if vim.v.shell_error == 0 then
+        return name .. branch
+      else
+        return name
+      end
+    end
+    vim.api.nvim_create_autocmd('VimEnter', {
+      callback = function()
+        -- Only load the session if nvim was started with no args
+        if vim.fn.argc(-1) == 0 then
+          resession.load(get_session_name(), { dir = 'dirsession', silence_errors = true })
+        end
+      end,
+    })
+    vim.api.nvim_create_autocmd('VimLeavePre', {
+      callback = function()
+        resession.save(get_session_name(), { dir = 'dirsession', notify = false })
+      end,
+    })
   end,
 }
