f5add0b93dfcf484dc7a5e80cf4f7f6d944f7404
shaun-mathew <shaunmathew123@gmail.com>
2024-02-18 20:41:45 -0500

    update nvim-cmp interface

--- modules/home-manager/development/editors/neovim/config/lua/cmp-setup.lua
+++ modules/home-manager/development/editors/neovim/config/lua/cmp-setup.lua
@@ -5,14 +5,66 @@ local luasnip = require 'luasnip'
 require('luasnip.loaders.from_vscode').lazy_load()
 luasnip.config.setup {}
 
+local kind_icons = {
+  Text = '',
+  Method = '󰆧',
+  Function = '󰊕',
+  Constructor = '',
+  Field = '󰇽',
+  Variable = '󰂡',
+  Class = '󰠱',
+  Interface = '',
+  Module = '',
+  Property = '󰜢',
+  Unit = '',
+  Value = '󰎠',
+  Enum = '',
+  Keyword = '󰌋',
+  Snippet = '',
+  Color = '󰏘',
+  File = '󰈙',
+  Reference = '',
+  Folder = '󰉋',
+  EnumMember = '',
+  Constant = '󰏿',
+  Struct = '',
+  Event = '',
+  Operator = '󰆕',
+  TypeParameter = '󰅲',
+}
+
 cmp.setup {
   snippet = {
     expand = function(args)
       luasnip.lsp_expand(args.body)
     end,
   },
+  preselect = 'None',
+  formatting = {
+    expandable_indicator = true,
+    format = function(entry, vim_item)
+      local lspkind_ok, lspkind = pcall(require, 'lspkind')
+      if not lspkind_ok then
+        -- From kind_icons array
+        vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatenates the icons with the name of the item kind
+        -- Source
+        vim_item.menu = ({
+          buffer = '[Buffer]',
+          nvim_lsp = '[LSP]',
+          luasnip = '[LuaSnip]',
+          nvim_lua = '[Lua]',
+          latex_symbols = '[LaTeX]',
+        })[entry.source.name]
+        return vim_item
+      else
+        -- From lspkind
+        return lspkind.cmp_format()(entry, vim_item)
+      end
+    end,
+  },
+
   completion = {
-    completeopt = 'menu,menuone,noinsert',
+    completeopt = 'menu,menuone,noinsert,noselect',
   },
   mapping = cmp.mapping.preset.insert {
     ['<C-n>'] = cmp.mapping.select_next_item(),
@@ -53,7 +105,46 @@ cmp.setup {
 
 -- `:` cmdline setup.
 cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
+  mapping = cmp.mapping.preset.cmdline {
+    ['<C-Space>'] = cmp.mapping.complete {},
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = {
+      c = function(_)
+        if cmp.visible() then
+          if #cmp.get_entries() == 1 then
+            cmp.confirm { select = true }
+          else
+            cmp.select_next_item()
+          end
+        else
+          cmp.complete()
+          if #cmp.get_entries() == 1 then
+            cmp.confirm { select = true }
+          end
+        end
+      end,
+    },
+
+    ['<S-Tab>'] = {
+      c = function(_)
+        if cmp.visible() then
+          if #cmp.get_entries() == 1 then
+            cmp.confirm { select = true }
+          else
+            cmp.select_prev_item()
+          end
+        else
+          cmp.complete()
+          if #cmp.get_entries() == 1 then
+            cmp.confirm { select = true }
+          end
+        end
+      end,
+    },
+  },
   sources = cmp.config.sources({
     { name = 'path' },
   }, {
