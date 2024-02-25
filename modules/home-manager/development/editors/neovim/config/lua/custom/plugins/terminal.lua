6de0bad2c9e79f6fb519d6103a0a961f6cc621b0
shaun-mathew <shaunmathew123@gmail.com>
2024-02-25 16:53:39 -0500

    specify git path when launching lazygit from nvim

--- modules/home-manager/development/editors/neovim/config/lua/custom/plugins/terminal.lua
+++ modules/home-manager/development/editors/neovim/config/lua/custom/plugins/terminal.lua
@@ -43,9 +43,31 @@ return {
     vim.keymap.set('n', '<leader>t]', term_map.cycle_next, { desc = 'Cycle next term' })
     vim.keymap.set('n', '<leader>t[', term_map.cycle_prev, { desc = 'Cycle prev term' })
 
+    local function find_git_root()
+      -- Use the current buffer's path as the starting point for the git search
+      local current_file = vim.api.nvim_buf_get_name(0)
+      local current_dir
+      local cwd = vim.fn.getcwd()
+      -- If the buffer is not associated with a file, return nil
+      if current_file == '' then
+        current_dir = cwd
+      else
+        -- Extract the directory from the current file's path
+        current_dir = vim.fn.fnamemodify(current_file, ':h')
+      end
+
+      -- Find the Git root directory from the current file's path
+      local git_root = vim.fn.systemlist('git -C ' .. vim.fn.escape(current_dir, ' ') .. ' rev-parse --show-toplevel')[1]
+      if vim.v.shell_error ~= 0 then
+        print 'Not a git repository. Searching on current working directory'
+        return cwd
+      end
+      return git_root
+    end
+
     local lazygit = require('terminal').terminal:new {
       layout = { open_cmd = 'float', height = 0.9, width = 0.9 },
-      cmd = { 'lazygit' },
+      cmd = { 'lazygit', '--path', find_git_root() },
       autoclose = true,
     }
     vim.env['GIT_EDITOR'] = "nvr -cc close -cc split --remote-wait +'set bufhidden=wipe'"
