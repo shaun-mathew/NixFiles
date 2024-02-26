d92f04e25b560157861d7891841b666db814bf54
shaun-mathew <shaunmathew123@gmail.com>
2024-02-26 14:35:45 -0500

    redo lsp-setup for neovim

--- modules/home-manager/development/editors/neovim/config/lua/lsp-setup.lua
+++ modules/home-manager/development/editors/neovim/config/lua/lsp-setup.lua
@@ -1,56 +1,49 @@
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
-local on_attach = function(_, bufnr)
-  -- NOTE: Remember that lua is a real programming language, and as such it is possible
-  -- to define small helper and utility functions so you don't have to repeat yourself
-  -- many times.
-  --
-  -- In this case, we create a function that lets us more easily define mappings specific
-  -- for LSP related items. It sets the mode, buffer and description for us each time.
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = 'LSP: ' .. desc
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+  callback = function(event)
+    local nmap = function(keys, func, desc)
+      vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
     end
 
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-  end
+    nmap('<leader>ca', function()
+      vim.lsp.buf.code_action { context = { only = { 'quickfix', 'refactor', 'source' } } }
+    end, '[C]ode [A]ction')
 
-  nmap('<leader>ca', function()
-    vim.lsp.buf.code_action { context = { only = { 'quickfix', 'refactor', 'source' } } }
-  end, '[C]ode [A]ction')
+    nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+    nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+    nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
 
-  nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+    nmap('<leader>lpd', require('goto-preview').goto_preview_definition, '[P]review [D]efinition')
+    nmap('<leader>lpt', require('goto-preview').goto_preview_type_definition, '[P]review [T]ype Definition')
+    nmap('<leader>lpi', require('goto-preview').goto_preview_implementation, '[P]review [I]mplementation')
+    nmap('<leader>lpD', require('goto-preview').goto_preview_declaration, '[P]review [D]eclaration')
+    nmap('<leader>lpp', require('goto-preview').close_all_win, 'Close Preview')
+    nmap('<leader>lpr', require('goto-preview').goto_preview_references, '[P]review References')
 
-  nmap('<leader>lpd', require('goto-preview').goto_preview_definition, '[P]review [D]efinition')
-  nmap('<leader>lpt', require('goto-preview').goto_preview_type_definition, '[P]review [T]ype Definition')
-  nmap('<leader>lpi', require('goto-preview').goto_preview_implementation, '[P]review [I]mplementation')
-  nmap('<leader>lpD', require('goto-preview').goto_preview_declaration, '[P]review [D]eclaration')
-  nmap('<leader>lpp', require('goto-preview').close_all_win, 'Close Preview')
-  nmap('<leader>lpr', require('goto-preview').goto_preview_references, '[P]review References')
+    nmap('<leader>ld', require('telescope.builtin').lsp_document_symbols, '[D]ocument Symbols')
+    nmap('<leader>lt', require('telescope.builtin').lsp_type_definitions, '[T]ype efinition')
+    nmap('<leader>ls', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+    nmap('<leader>lr', vim.lsp.buf.rename, '[R]ename')
 
-  nmap('<leader>ld', require('telescope.builtin').lsp_document_symbols, '[D]ocument Symbols')
-  nmap('<leader>lt', require('telescope.builtin').lsp_type_definitions, '[T]ype efinition')
-  nmap('<leader>ls', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-  nmap('<leader>lr', vim.lsp.buf.rename, '[R]ename')
+    -- See `:help K` for why this keymap
+    nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
 
-  -- See `:help K` for why this keymap
-  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-
-  -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>lwa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>lwr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>lwl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
-end
+    -- Lesser used LSP functionality
+    nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+    nmap('<leader>lwa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+    nmap('<leader>lwr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+    nmap('<leader>lwl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, '[W]orkspace [L]ist Folders')
+  end,
+})
 
 -- mason-lspconfig requires that these setup functions are called in this order
 -- before setting up the servers.
 require('mason').setup()
-require('mason-lspconfig').setup()
 
 -- Enable the following language servers
 --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -70,38 +63,56 @@ local servers = {
   html = { filetypes = { 'html', 'twig', 'hbs' } },
 
   lua_ls = {
-    Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
-      -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-      -- diagnostics = { disable = { 'missing-fields' } },
+    -- cmd = {...},
+    -- filetypes { ...},
+    -- capabilities = {},
+    settings = {
+      Lua = {
+        runtime = { version = 'LuaJIT' },
+        workspace = {
+          checkThirdParty = false,
+          -- Tells lua_ls where to find all the Lua files that you have loaded
+          -- for your neovim configuration.
+          library = {
+            '${3rd}/luv/library',
+            unpack(vim.api.nvim_get_runtime_file('', true)),
+          },
+          telemetry = { enable = false },
+          -- If lua_ls is really slow on your computer, you can try this instead:
+          -- library = { vim.env.VIMRUNTIME },
+        },
+        -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+        -- diagnostics = { disable = { 'missing-fields' } },
+      },
     },
   },
 }
 
--- Setup neovim lua configuration
-require('neodev').setup()
-
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
--- Ensure the servers above are installed
-local mason_lspconfig = require 'mason-lspconfig'
+local ensure_installed = vim.tbl_keys(servers or {})
+vim.list_extend(ensure_installed, {
+  'stylua', -- Used to format lua code
+})
 
-mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
-}
-
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-      filetypes = (servers[server_name] or {}).filetypes,
-    }
-  end,
+require('mason-lspconfig').setup {
+  ensure_installed = ensure_installed,
+  handlers = {
+    function(server_name)
+      local server = servers[server_name] or {}
+      require('lspconfig')[server_name].setup {
+        cmd = server.cmd,
+        settings = server.settings,
+        filetypes = server.filetypes,
+        -- This handles overriding only values explicitly passed
+        -- by the server configuration above. Useful when disabling
+        -- certain features of an LSP (for example, turning off formatting for tsserver)
+        capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {}),
+      }
+    end,
+  },
 }
 
 -- vim: ts=2 sts=2 sw=2 et
